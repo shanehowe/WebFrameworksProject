f6419288716879966e446e457c02bc66ec898dbc
Shane Howe <shanehowe100@hotmail.com>
2024-09-19 13:30:14 +0100

    first commit

---
+++ app/controllers/auth.js
@@ -0,0 +1,13 @@
+
+/**
+ * Renders the login page.
+ *
+ * This function is an Express.js route handler that serves the login page to the client.
+ *
+ * @param {import("express").Request} request - The Express request object.
+ * @param {import("express").Response} response - The Express response object.
+ */
+function renderLogInPage(request, response) {
+
+  
+}
